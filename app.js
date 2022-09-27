c7150e455885307a19511b8a14ae527e8d29b026
Yasir Khan <khanyaser007@gmail.com>
2022-09-27 17:37:56 +0500

    Update app.js file for local storage

--- app.js
+++ app.js
@@ -1,59 +1,25 @@
-// class Book {
-//   constructor (title, author) {
-//     this.title = title;
-//     this.author = author;
-//   }
-// }
-// const awesomeBooks = [];
+const list = document.getElementById('lists');
+const form = document.querySelector('form');
+const author = document.getElementById('atr');
+const title = document.getElementById('ttl');
 
-// awesomeBooks.push(new Book(title.value, author.value));
+const bookCollection = [];
 
-// console.log(awesomeBooks);
-
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const listBooks = document.querySelector('.container ul');
-const myButton = document.querySelector('#btn-list');
-let index = 0;
-
-myButton.addEventListener('click', (e) => {
-  if (title.value !== '' && author.value !== '') {
-    e.preventDefault();
-    index += 1;
-
-    const titleBooks = document.createElement('li');
-    titleBooks.innerHTML = title.value;
-    listBooks.appendChild(titleBooks);
-
-    const authorBooks = document.createElement('li');
-    authorBooks.innerHTML = author.value;
-    listBooks.appendChild(authorBooks);
-
-    const remove = document.createElement('button');
-
-    // Event: Remove a Book
-    remove.innerText = 'Remove';
-    listBooks.appendChild(remove);
-    remove.setAttribute('id', `remove-${index}`);
-    const hr = document.createElement('hr');
-    listBooks.appendChild(hr);
-    const button = document.querySelector(`#remove-${index}`);
-    button.addEventListener('click', () => {
-      titleBooks.remove();
-      authorBooks.remove();
-      remove.remove();
-      hr.remove();
-    });
-  }
-});
-
-// Local storage
-const bookForm = document.querySelector('#addBook');
-const content = JSON.parse(window.localStorage.getItem('storage'));
-function check() {
-  if (localStorage.getItem('storage')) {
-    bookForm.elements.title.value = content.title;
-    bookForm.elements.author.value = content.author;
-  }
+function collection(title, author) {
+  this.title = title;
+  this.author = author;
 }
-window.onload = check;
+
+function alocalStorage() {
+  const key = title.value;
+  localStorage.setItem(key, JSON.stringify(bookCollection));
+}
+
+function newCollection() {
+  // eslint-disable-next-line new-cap
+  const newCollectionList = new collection(title.value, author.value);
+  bookCollection.push(newCollectionList);
+  alocalStorage();
+}
+
+form.addEventListener('submit', newCollection);
