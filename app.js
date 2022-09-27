5b3b003b0052de3dc17497ae1a3899bc06aadf8d
Olivier-Kango <olivierkango@gmail.com>
2022-09-27 14:47:34 -0700

    Add local storage function

--- app.js
+++ app.js
@@ -23,3 +23,36 @@ function newCollection() {
 }
 
 form.addEventListener('submit', newCollection);
+
+function dataOutput() {
+  Object.keys(localStorage).forEach((key) => {
+    const dataForLocalstorage = JSON.parse(localStorage.getItem(key));
+    if (localStorage) {
+      dataForLocalstorage.forEach((collection) => {
+        const li = document.createElement('li');
+        const theBookTitle = document.createElement('span');
+        const theBookAuthor = document.createElement('span');
+        const deleteBtn = document.createElement('button');
+
+        deleteBtn.textContent = 'Remove';
+        deleteBtn.classList.add('Remove');
+        theBookTitle.textContent = collection.title;
+        theBookAuthor.textContent = collection.author;
+        deleteBtn.addEventListener('click', (e) => {
+          const key = collection.title;
+          localStorage.removeItem(key);
+          e.target.parentNode.remove();
+        });
+        li.classList.add('newCollection');
+        theBookTitle.style.display = 'block';
+        theBookAuthor.style.display = 'block';
+        li.appendChild(theBookTitle);
+        li.appendChild(theBookAuthor);
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
+      });
+    }
+  });
+}
+
+dataOutput();
