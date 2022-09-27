b178d1a982cf563bcd78745c122a51a9e2cb9be4
Olivier-Kango <olivierkango@gmail.com>
2022-09-26 17:15:55 -0700

    Create a function to add a new book to the collection, with title and author.

---
+++ app.js
@@ -0,0 +1,32 @@
+class Book {
+  constructor (title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const listBooks = document.querySelector('.container ul');
+const myButton = document.querySelector('#btn-list');
+
+myButton.addEventListener('click', (e) => {
+  if(title.value != "" && author != "") {
+    e.preventDefault();
+
+    const titleBooks = document.createElement('li');
+    titleBooks.innerHTML = title.value;
+    listBooks.appendChild(titleBooks);
+
+    const authorBooks = document.createElement('li');
+    authorBooks.innerHTML = author.value;
+    listBooks.appendChild(authorBooks);
+
+    const remove = document.createElement('button');
+    remove.innerText = 'Remove'
+    listBooks.appendChild(remove);
+
+    const hr = document.createElement('hr');
+    listBooks.appendChild(hr);
+  }
+})
