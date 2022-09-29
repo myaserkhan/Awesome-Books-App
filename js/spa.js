47453500b53dfc6facb202d26646d77c49202774
Olivier-Kango <olivierkango@gmail.com>
2022-09-29 11:27:54 -0700

    Add and remove Divs when we click on butons

--- js/spa.js
+++ js/spa.js
@@ -9,3 +9,36 @@ const dateFormat = {
   second: '2-digit',
 };
 currentDateTime.textContent = currentDate.toLocaleDateString('en-US', dateFormat);
+
+const addNew = document.querySelector('#link-add');
+const linkList = document.querySelector('#link-list');
+const linkContact = document.querySelector('#link-contact');
+const divList = document.querySelector('.container-list');
+const divAdd = document.querySelector('#books');
+const divContact = document.querySelector('.div-contact');
+
+divContact.style.background = 'red';
+
+addNew.addEventListener('click', () => {
+  divList.style.display = 'none';
+  divContact.style.display = 'none';
+  addNew.style.color = '#006abc';
+  linkList.style.color = '#000';
+  linkContact.style.color = '#000';
+});
+
+linkList.addEventListener('click', () => {
+  divAdd.style.display = 'none';
+  divContact.style.display = 'none';
+  linkList.style.color = '#006abc';
+  addNew.style.color = '#000';
+  linkContact.style.color = '#000';
+});
+
+linkContact.addEventListener('click', () => {
+  divList.style.display = 'none';
+  divAdd.style.display = 'none';
+  linkContact.style.color = '#006abc';
+  addNew.style.color = '#000';
+  linkList.style.color = '#000';
+});
